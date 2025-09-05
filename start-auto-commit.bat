25b369d8e7a99aeb273bcff98da1e822f42eebd7
Harmit Patel <harmitpatel428@gmail.com>
2025-09-05 23:36:25 +0530

    Add auto-commit system for automatic file monitoring and Git commits

---
+++ start-auto-commit.bat
@@ -0,0 +1,10 @@
+@echo off
+echo 🚀 Starting Sales Funnel Auto-Commit System...
+echo.
+echo This will automatically commit and push any changes you make to your files.
+echo Press Ctrl+C to stop the auto-commit system.
+echo.
+
+node auto-commit.js
+
+pause
